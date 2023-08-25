0611aaa043e7cddebd76987bd518d68c5ebe6ffc
Robert Bubnik <rbubnik@gmail.com>
2023-08-25 18:35:32 -0400

    displays book information when clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(`http://localhost:3000/books`)
+    .then(response => response.json())
+    .then(data => populateBooks(data))
+
+});
+
+const list = document.getElementById('list')
+const bookImage = document.createElement('img')
+const bookDescription = document.createElement('p')
+const bookUsersLiked = document.createElement('ul')
+const bookPanel = document.getElementById('show-panel')
+bookPanel.appendChild(bookImage)
+bookPanel.appendChild(bookDescription)
+bookPanel.appendChild(bookUsersLiked)
+
+function populateBooks (data) {
+    console.log(data)
+    data.forEach(book => {
+        const title = document.createElement('li')
+        const bookInfo = book
+    title.textContent = book.title
+        list.appendChild(title)
+        title.addEventListener('click', (e) =>{
+            e.preventDefault()
+            displayBook(book)
+        })
+    })
+}
+
+
+
+function displayBook (book) {
+    console.log(book)
+     // /display the book's thumbnail, description, and a list of users who have liked the book. This information should be displayed in the div#show-panel element.
+    
+    bookImage.src = book.img_url
+    bookDescription.textContent = book.description 
+    showUsers(book.users)
+
+
+   
+
+
+}
+
+function showUsers (users) {
+    bookUsersLiked.innerHTML =''
+    return users.forEach(user => {
+        const userLi = document.createElement('li')
+        userLi.textContent = user.username
+        bookUsersLiked.appendChild(userLi)
+    })
+}
+
+//next step like a book
